85cab9cd534e0e4e710637baf6ae3802c665dff8
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-02 16:44:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,39 +12,45 @@ openai.api_key = st.secrets["chat_gpt_key"]
 df = pd.read_csv('performance.csv')
 
 def openai_response(query):
-   response = openai.ChatCompletion.create(
-   model="gpt-3.5-turbo",
-
-   messages = [
-       {"role":"system", "content":"You are helpful assistant."},
-       {"role":"user","content": query}
-   ]
-   )
-   return response.choices[0]['message']['content']   
-    
+	response = openai.ChatCompletion.create(
+	model="gpt-3.5-turbo",
+	messages = [
+		{"role":"system", "content":"You are helpful assistant."},
+		{"role":"user","content": query}
+	]
+	)
+	return response.choices[0]['message']['content']  
+	
+def row_converter(row, listy):
+	#convert pandas row to a dictionary
+	#requires a list of columns and a row as a tuple
+	count = 1
+	pictionary = {}
+	pictionary['Index'] = row[0]
+	for item in listy:
+		pictionary[item] = row[count]
+		count += 1
+	print(pictionary)
+	return pictionary
+	
 with st.sidebar:
-    st.image(image, width = 150)
-    st.header('Generative AI')
+st.image(image, width = 150)
+st.header('Generative AI')
 
 
 st.header("Personalized communication ")
 
 with st.form("my_form"):
-   name = st.selectbox('Please select name',df["name"])
-   intent_of_mail = st.text_input(label ="Intent of mail" , placeholder = 'Intent')
-   category = df[df.name == name]['performance'].to_string(index=False)
-   target = df[df.name == name]['target'].to_string(index=False)
-   latest_performance = df[df.name == name]['latest_month_performance'].to_string(index=False)
-   
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-    response = openai_response(f"""Write a {intent_of_mail} mail to a salesperson {name} as their employer based on following information starts and ends with triplle dashes marks,
-Analyse the data to determine whether a salesperson's performance is above or below target and how it impacts the performance category,
-offer some insight based on performance and their category,
---- {name} is {category} with their target, their latest target was {target} and latest performance was {latest_performance} ---
-""")
-    st.text(f"""Category : {category}\nTarget : {target}\nLatest performance : {latest_performance}""")
-    st.write()
-    st.markdown(response)
-
+	name = st.selectbox('Please select name',df['performance'])
+	intent_of_mail = st.text_input(label ="Intent of mail" , placeholder = 'Intent')
+	category = df[df.name == name]['performance'].to_string(index=False)
+	target = df[df.name == name]['target'].to_string(index=False)
+	latest_performance = df[df.name == name]['latest_month_performance'].to_string(index=False)
+	
+	# Every form must have a submit button.
+	submitted = st.form_submit_button("Submit")
+	if submitted:
+		listy = df.columns
+		for row in df.itertuples():
+			rowDict = row_converter(row, listy)
+		st.markdown(rowDict)
