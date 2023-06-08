584c7077a3f5aadbfcec0eb47ed2c00fa044c99b
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-08 14:58:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,9 +79,9 @@ with st.form("my_form"):
 			dicts_to_csv(res[i], 'mails_data.csv')
 			st.dataframe(pd.read_csv('mails_data.csv', sep = '|'))
 			with open("mails_data.csv", "rb") as file:
-			st.download_button(
-				label="Download data as CSV",
-				data=file,
-				file_name='large_df.csv',
-				mime='text/csv',
-			)
+				st.download_button(
+					label="Download data as CSV",
+					data=file,
+					file_name='large_df.csv',
+					mime='text/csv',
+				)
