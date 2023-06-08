f3267645d3f3da01365b4f99e53acbf640520c21
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-08 19:20:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,8 @@ st.header("Personalized communication ")
 
 
 if st.button("generate"):
-	for i in range(len(df)):
-		df2 = df.iloc[[i]]
+	for i in range(1):#len(df)):
+		df2 = df.loc[i]
 		#df2 = df.loc[i].to_frame().T
 		data = df2.to_dict()
 		#st.markdown(data)
@@ -57,40 +57,40 @@ if st.button("generate"):
 		
 		#based on performance category choose query
 		check_category = df2.loc[i, 'Category']
-		if check_category == 'Consistent Performer':
-			query = """Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
-				analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance category, congratulate and motivate them along with motivational quotes,
-				offer some insight based on their performance
-				Please keep the mail concise and start with their name, sign it as 'Manager'
-				"""
-		elif check_category == 'Consistent Non-performer':
-			query ="""Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
-				analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance, suggesting improvement areas, and it should include 2 sales trainng article or link references based on the performance and category
-				also include motivational quotes to motivate them
-				Please keep the mail concise and start with their name, sign it as 'Manager'
-				""" 
-		elif check_category == 'Performer to Non-performer':
-			query = """Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
-				analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance as their performance recentley dropped, suggesting improvement areas, and it should include 2 sales trainng article or link references based on the performance to improve it
-				also include motivational quotes to motivate them
-				Please keep the mail concise and start with their name, sign it as 'Manager'
-				"""
-		elif check_category == 'Non-performer to Performer':
-			query = """Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
-				analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance as their performance recentley improved, suggesting improvement areas, and it should include 2 sales trainng article or link references based on the performance to further improve it
-				also include motivational quotes to motivate them
-				Please keep the mail concise and start with their name, sign it as 'Manager'
-				"""
-		else:
-			query = 'show message : category is not correct'
-			
-		response = openai_response(query + f"""\ndata: ```{data} ``` """)
-		#st.markdown(response)
-		
-		df2['Mail'] = response
-		#st.write(df2)
-		for row in dataframe_to_rows(df2, index=False, header=False):
-    			sheet.append(row)
+		#if check_category == 'Consistent Performer':
+		#	query = """Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
+		#		analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance category, congratulate and motivate them along with motivational quotes,
+		#		offer some insight based on their performance
+		#		Please keep the mail concise and start with their name, sign it as 'Manager'
+		#		"""
+		#elif check_category == 'Consistent Non-performer':
+		#	query ="""Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
+		#		analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance, suggesting improvement areas, and it should include 2 sales trainng article or link references based on the performance and category
+		#		also include motivational quotes to motivate them
+		#		Please keep the mail concise and start with their name, sign it as 'Manager'
+		#		""" 
+		#elif check_category == 'Performer to Non-performer':
+		#	query = """Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
+		#		analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance as their performance recentley dropped, suggesting improvement areas, and it should include 2 sales trainng article or link references based on the performance to improve it
+		#		also include motivational quotes to motivate them
+		#		Please keep the mail concise and start with their name, sign it as 'Manager'
+		#		"""
+		#elif check_category == 'Non-performer to Performer':
+		#	query = """Your task is to write atleast 250 word mail to insurance agent about their performance data delimited by three backticks,
+		#		analysing performance using their total sales, target and sales achieved percentages, give feedback based on performance as their performance recentley improved, suggesting improvement areas, and it should include 2 sales trainng article or link references based on the performance to further improve it
+		#		also include motivational quotes to motivate them
+		#		Please keep the mail concise and start with their name, sign it as 'Manager'
+		#		"""
+		#else:
+		#	query = 'show message : category is not correct'
+		#	
+		#response = openai_response(query + f"""\ndata: ```{data} ``` """)
+		##st.markdown(response)
+		#
+		#df2['Mail'] = response
+		##st.write(df2)
+		#for row in dataframe_to_rows(df2, index=False, header=False):
+    		#	sheet.append(row)
 	
 	workbook.save('output1.xlsx')
 	with open("output1.xlsx", "rb") as file:
